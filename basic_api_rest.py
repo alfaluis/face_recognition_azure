a3e2e7998b0b7b906382c83ba13efd1319f60fb8
alfaluis <alfa.b14@gmail.com>
2020-10-02 15:19:47 -0300

    Basic code for face detection service with API Request

--- basic_api_rest.py
+++ basic_api_rest.py
@@ -1,65 +1,40 @@
-import requests
-import json
-import cv2
 import os
+import argparse
+import utils
 
-ENDPOINT = 'https://face-api-dev.cognitiveservices.azure.com/'
-
-
-def get_rectangle(face_dictionary):
-    rect = face_dictionary['faceRectangle']
-    left = rect['left']
-    top = rect['top']
-    right = left + rect['width']
-    bottom = top + rect['height']
-    return (left, top), (right, bottom)
-
-
-def param_config(face_attributes):
-    if face_attributes == '':
-        params = {'returnFaceId': 'true', 'returnFaceLandmarks': 'false'}
-    else:
-        params = {'returnFaceId': 'true', 'returnFaceLandmarks': 'false', 'returnFaceAttributes': face_attributes}
-    return params
-
-
-def detect_face_stream(key, image, face_attributes=''):
-    face_api_url = ENDPOINT + 'face/v1.0/detect'
-    headers = {'Ocp-Apim-Subscription-Key': key, 'Content-Type': 'application/octet-stream'}
-    params = param_config(face_attributes)
-    response = requests.post(face_api_url, params=params,
-                             headers=headers, data=image)
-    return response.json()
-
-
-def detect_face_url(key, image_url, face_attributes=''):
-    face_api_url = ENDPOINT + 'face/v1.0/detect'
-    headers = {'Ocp-Apim-Subscription-Key': key, 'Content-Type': 'application/json'}
-    params = param_config(face_attributes)
-    response = requests.post(face_api_url, params=params,
-                             headers=headers, json={"url": image_url})
-    return response.json()
+import cv2
 
 
 if __name__ == '__main__':
-    KEY = '298b3b2660164139b5b0be02d2c8c219'
-    image_path = os.path.join(os.getcwd(), 'data', 'president-family.jpg')
-    # attributes = 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-    attributes = ''
+    # configure arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-k", "--KEY", required=True, help="Access key of endpoint")
+    ap.add_argument("-ep", "--ENDPOINT", required=True, help="Endpoint to the face service")
+    args = vars(ap.parse_args())
 
-    # detect faces in image
+    # configure the face client
+    KEY = args['KEY']
+    ENDPOINT = args['ENDPOINT']
+
+    # load an image
+    image_path = os.path.join(os.getcwd(), 'data', 'president-family.jpg')
     local_image = cv2.imread(image_path)
     img = cv2.imencode('.jpg', local_image)[1].tobytes()
-    detected_faces = detect_face_stream(key=KEY, image=img, face_attributes=attributes)
+
+    # Function to call the API REST
+    attributes = ''
+    # attributes = 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+    detected_faces = utils.detect_face_stream(endpoint=ENDPOINT, key=KEY, image=img, face_attributes=attributes)
 
     # Display the resulting frame
-    print(detected_faces)
     color = (255, 0, 0)
     thickness = 2
     for face in detected_faces:
-        local_image = cv2.rectangle(local_image, *get_rectangle(face), color, thickness)
+        print(face)
+        local_image = cv2.rectangle(local_image, *utils.get_rectangle(face), color, thickness)
 
     cv2.imshow('frame', local_image)
     cv2.waitKey()
+
     # Destroy all the windows
     cv2.destroyAllWindows()
